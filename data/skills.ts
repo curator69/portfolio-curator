046927c3a6e2eac00831b0f57fa6d301a4a56547
Rushikesh <rushikeshkhillari10@gmail.com>
2025-04-07 01:48:59 +0530

    updated skills

--- data/skills.ts
+++ data/skills.ts
@@ -11,11 +11,29 @@ import nextjs from "../public/icons/skills/nextjs.svg";
 import nodejs from "../public/icons/skills/nodejs.svg";
 import react from "../public/icons/skills/react.svg";
 import redux from "../public/icons/skills/redux.svg";
+import vuex from "../public/icons/skills/vuex.svg";
 import typescript from "../public/icons/skills/typescript.svg";
 import tailwindcss from "../public/icons/skills/tailwindcss.svg";
 import sass from "../public/icons/skills/sass.svg";
-import vuejs from "../public/icons/skills/vuejs.svg";
+import vuejs from "../public/icons/skills/vuejs.png";
 import nuxtjs from "../public/icons/skills/nuxtjs.svg";
+import docker from "../public/icons/skills/docker.webp";
+import kubernetes from "../public/icons/skills/Kubernetes.png";
+import golang from "../public/icons/skills/golang.png";
+import python from "../public/icons/skills/python.png";
+import mysql from "../public/icons/skills/mysql.svg";
+import postgresql from "../public/icons/skills/postgresql.svg";
+import redis from "../public/icons/skills/redis.svg";
+import rabbitmq from "../public/icons/skills/rabbitmq.svg";
+import graphql from "../public/icons/skills/GraphQL.png";
+import aws from "../public/icons/skills/aws.png";
+import azure from "../public/icons/skills/azure.png";
+import gcp from "../public/icons/skills/gcp.png";
+import firebase from "../public/icons/skills/firebase.svg";
+import postman from "../public/icons/skills/postman.svg";
+import figma from "../public/icons/skills/figma.svg";
+import nginx from "../public/icons/skills/nginx.svg";
+import linux from "../public/icons/skills/linux.png";
 
 export const skills = [
   {
@@ -29,9 +47,149 @@ export const skills = [
     type: "Frontend",
   },
   {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: vuejs,
+    name: "VueJs",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nuxtjs,
+    name: "NuxtJs",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: vuex,
+    name: "Vuex",
+    type: "State Management",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
     imageUrl: express,
     name: "Express",
     type: "Backend",
+  },
+  {
+    imageUrl: nestjs,
+    name: "NestJs",
+    type: "Backend",
+  },
+  {
+    imageUrl: python,
+    name: "Python",
+    type: "Backend",
+  },
+  {
+    imageUrl: golang,
+    name: "Golang",
+    type: "Backend",
+  },
+  {
+    imageUrl: graphql,
+    name: "GraphQL",
+    type: "Backend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: mysql,
+    name: "MySQL",
+    type: "Database",
+  },
+  {
+    imageUrl: postgresql,
+    name: "PostgreSQL",
+    type: "Database",
+  },
+  {
+    imageUrl: redis,
+    name: "Redis",
+    type: "Database",
+  },
+  {
+    imageUrl: firebase,
+    name: "Firebase",
+    type: "Database",
+  },
+  {
+    imageUrl: docker,
+    name: "Docker",
+    type: "DevOps",
+  },
+  {
+    imageUrl: kubernetes,
+    name: "Kubernetes",
+    type: "DevOps",
+  },
+  {
+    imageUrl: aws,
+    name: "AWS",
+    type: "DevOps",
+  },
+  {
+    imageUrl: azure,
+    name: "Azure",
+    type: "DevOps",
+  },
+  {
+    imageUrl: gcp,
+    name: "GCP",
+    type: "DevOps",
+  },
+  {
+    imageUrl: nginx,
+    name: "Nginx",
+    type: "DevOps",
+  },
+  {
+    imageUrl: linux,
+    name: "Linux",
+    type: "DevOps",
   },
   {
     imageUrl: git,
@@ -44,68 +202,18 @@ export const skills = [
     type: "Version Control",
   },
   {
-    imageUrl: javascript,
-    name: "JavaScript",
-    type: "Frontend",
+    imageUrl: postman,
+    name: "Postman",
+    type: "Tools",
   },
   {
-    imageUrl: mongodb,
-    name: "MongoDB",
-    type: "Database",
+    imageUrl: figma,
+    name: "Figma",
+    type: "Tools",
   },
   {
-    imageUrl: nestjs,
-    name: "NestJs",
-    type: "Backend",
-  },
-  {
-    imageUrl: mui,
-    name: "Material-UI",
-    type: "Frontend",
-  },
-  {
-    imageUrl: nextjs,
-    name: "Next.js",
-    type: "Frontend",
-  },
-  {
-    imageUrl: nodejs,
-    name: "Node.js",
-    type: "Backend",
-  },
-  {
-    imageUrl: react,
-    name: "React",
-    type: "Frontend",
-  },
-  {
-    imageUrl: redux,
-    name: "Redux",
-    type: "State Management",
-  },
-  {
-    imageUrl: sass,
-    name: "Sass",
-    type: "Frontend",
-  },
-  {
-    imageUrl: tailwindcss,
-    name: "Tailwind CSS",
-    type: "Frontend",
-  },
-  {
-    imageUrl: typescript,
-    name: "TypeScript",
-    type: "Frontend",
-  },
-  {
-    imageUrl: vuejs,
-    name: "VueJs",
-    type: "Frontend",
-  },
-  {
-    imageUrl: nuxtjs,
-    name: "NuxtJs",
-    type: "Frontend",
+    imageUrl: rabbitmq,
+    name: "RabbitMQ",
+    type: "Tools",
   },
 ];
